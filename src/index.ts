befeef44e990d10f20f4c1cb8fd75fd2763d4e2b
Dan Kimutai <dankimutai03@gmail.com>
2026-04-02 00:22:21 +0300

    add netlify cors

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ const allowedOrigins = [
   'http://localhost:5174',
   'http://localhost:3000',
   'https://smartfarm-sys-b89d3c.netlify.app',
+  'https://reliable-jelly-003a81.netlify.app',
   'https://smartfarm-production-d317.up.railway.app'
 ];
 // Add middleware
